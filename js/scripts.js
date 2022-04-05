7cd321e65ec1262d71657f9460698d88c1022609
patty-rose <oteropatty@gmail.com>
2022-04-04 20:37:25 -0700

    add user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic:
+
 function add(number1, number2){
 	return number1 + number2;
 }
@@ -14,7 +16,38 @@ function divide(number1, number2){
   return number1 / number2;
 }
 
-const number1 = parseInt(prompt("enter a number:"));
-const number2 = parseInt(prompt("enter another number:"));
+//User Interface Logic
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
 
-alert(divide(number1, number2));
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#numSub1").val());
+    const number2 = parseInt($("#numSub2").val());
+    const result = subtract(number1, number2);
+    $("#outputSubtract").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = multiply(number1, number2);
+    $("#outputMultiply").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#num1").val());
+    const number2 = parseInt($("#num2").val());
+    const result = divide(number1, number2);
+    $("#outputDivide").text(result);
+  });
+
+});
